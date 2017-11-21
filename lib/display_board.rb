b6b35f1e66c4abd17cda2348a59884b577566043
Mai Nguyen <m.nguyen-17@student.lboro.ac.uk>
2017-11-21 21:31:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  space = "   "
-  board = "#{space}, |, #{space}, |, #{space}"
-  puts board
-  puts board
-  puts board
+  puts "   |   |   "
+  11.times { print "-" }
+  puts ""
+  puts "   |   |   "
+  11.times { print "-" }
+  puts ""
+  puts "   |   |   "
 end
+display_board
